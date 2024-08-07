b39de982d7a24b32bb4c99a5312c8bc198e549af
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-07 19:18:21 -0300

    Create KafkaConsumerConfig.java

---
+++ src/main/java/tech/noetzold/SensorCenter/config/KafkaConsumerConfig.java
@@ -0,0 +1,40 @@
+package tech.noetzold.SensorCenter.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "general-api-group");
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        configProps.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    @Bean
+    public ConcurrentMessageListenerContainer<String, Object> kafkaListenerContainerFactory() {
+        ContainerProperties containerProps = new ContainerProperties("processed-data-topic");
+        containerProps.setAckMode(ContainerProperties.AckMode.MANUAL);
+        return new ConcurrentMessageListenerContainer<>(consumerFactory(), containerProps);
+    }
+}
