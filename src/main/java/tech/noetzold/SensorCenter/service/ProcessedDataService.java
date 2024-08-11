4702ea30447a7193d307fcf7f3080d981f58869d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-10 21:10:50 -0300

    Update ProcessedDataService.java

--- src/main/java/tech/noetzold/SensorCenter/service/ProcessedDataService.java
+++ src/main/java/tech/noetzold/SensorCenter/service/ProcessedDataService.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Service;
 import tech.noetzold.SensorCenter.model.ProcessedData;
 import tech.noetzold.SensorCenter.repository.ProcessedDataRepository;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProcessedDataService {
@@ -23,7 +27,33 @@ public class ProcessedDataService {
     }
 
     @KafkaListener(topics = "${spring.kafka.topic.processed-data}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consume(ProcessedData processedData) {
+    public void consume(Map<String, Object> data) {
+        // Converte o Map recebido em um objeto ProcessedData
+        ProcessedData processedData = convertMapToProcessedData(data);
         saveProcessedData(processedData);
+    }
+
+    private ProcessedData convertMapToProcessedData(Map<String, Object> data) {
+        ProcessedData processedData = new ProcessedData();
+        processedData.setSensorType((String) data.get("sensorType"));
+        processedData.setValue((Double) data.get("value"));
+        processedData.setCoordinates((String) data.get("coordinates"));
+
+        // Converte o ArrayList de timestamp para LocalDateTime
+        List<Integer> timestamp = (List<Integer>) data.get("timestamp");
+        if (timestamp != null && timestamp.size() == 7) {
+            LocalDateTime dateTime = LocalDateTime.of(
+                    timestamp.get(0),  // Ano
+                    timestamp.get(1),  // Mês
+                    timestamp.get(2),  // Dia
+                    timestamp.get(3),  // Hora
+                    timestamp.get(4),  // Minuto
+                    timestamp.get(5),  // Segundo
+                    timestamp.get(6)   // Milissegundo
+            );
+            processedData.setTimestamp(dateTime);
+        }
+
+        return processedData;
     }
 }
